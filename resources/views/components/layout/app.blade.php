13ff962e755b5eda7cfebe8bc5c858b1b363609e
Marcos Cantarutti <marcossandim21@gmail.com>
2024-12-17 20:06:58 -0300

    iniciado

--- resources/views/components/layout/app.blade.php
+++ resources/views/components/layout/app.blade.php
@@ -6,8 +6,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{config('app.name')}}</title>
+    @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 
-    @vite(['resources/css/app.css'])
 </head>
 
 <body class="bg-base-900  h-full">
