531e27a7b91d42d62d122c6a9ad6e10c08880f40
Marcos Cantarutti <marcossandim21@gmail.com>
2024-12-17 20:21:09 -0300

    iniciado

--- resources/views/components/layout/app.blade.php
+++ resources/views/components/layout/app.blade.php
@@ -7,7 +7,6 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{config('app.name')}}</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
-
 </head>
 
 <body class="bg-base-900  h-full">
